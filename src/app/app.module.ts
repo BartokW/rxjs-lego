af1ac7fb0b473a3b2bfffa4184e1e761465e3e44
BartokW <jonw@warringtonz.ca>
2022-06-29 13:20:19 -0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { ImageCropperModule } from 'ngx-image-cropper';
 
 @NgModule({
